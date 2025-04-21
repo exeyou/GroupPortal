6d4439de4b38063993ba9b9b0c023d56a30b8f8a
exeyou <arsenstukalov@gmail.com>
2025-04-21 19:38:26 +0300

    Merged forum and gallery

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portal',
-    'accounts',
-    'forum',
     "crispy_forms",
     "crispy_bootstrap5",
+    'accounts',
+    'forum',
     'gallery',
 ]
 
